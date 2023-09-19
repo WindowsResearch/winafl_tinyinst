03a725c0bfebea6fcbaec2d4b8f666c1fb4d11ed
a1gx <a1gx@192.168.5.73>
2023-09-19 10:24:59 +0800

    修改运行命令

--- run.bat
+++ run.bat
@@ -11,7 +11,7 @@ for /f "tokens=2 delims=," %%a in ('tasklist /nh /fi "imagename eq %process_name
 
 :end
 if defined pid (
-    afl-fuzz.exe -A %pid% -i in -o out -y -t 20000+ -l custom_net_fuzzer.dll -- -instrument_module mqqm.dll -target_module mqqm.dll -target_offset 0x5b970 -nargs 1 -iterations 5000 -persist -trace_debug_events -- test_gdiplus.exe @@
+    afl-fuzz.exe -A %pid% -i in -o out -y -t 20000+ -l custom_net_fuzzer.dll -- -instrument_module mqqm.dll -target_module mqqm.dll -target_offset 0x5b970 -nargs 1 -patch_return_addresses -iterations 5000 -persist -trace_debug_events -- test_gdiplus.exe @@
 ) else (
     echo Process %process_name% not found
 )
